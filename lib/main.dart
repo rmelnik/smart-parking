ae47a9f2a220e94e07f3fe4b0e5dad92b5c6f806
unknown <romanmelnik@gmail.com>
2021-05-01 20:58:34 +0200

    Changed text style, implemented hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Smart Parking'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-       Text('Skriv in ditt registreringsnummer'),
-
-      Text('Skanna ditt registreringsnummer'),
-      ]
-    ),
-  ),
-
-  //floatingActionButton: FloatingActionButton(
-  //onPressed: () {  },
-  //child: Icon(Icons.camera_alt_rounded),
-//Icons.camera_alt_rounded
-),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Smart Parking'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                  'Skriv in ditt registreringsnummer',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  )
+              ),
+
+              Text('Skanna ditt registreringsnummer'),
+            ]
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.camera_alt_rounded),
+        backgroundColor: Colors.red[600],
+        //Icons.camera_alt_rounded,
+      ),
+    );
+  }
+}
